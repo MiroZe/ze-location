686627290aaa4d1ea1654a7194e25a926f50ee8b
Miroslav DImitrov <114018004+MiroZe@users.noreply.github.com>
2024-09-25 15:00:58 +0300

    process login

--- src/components/Auth/Login/Login.jsx
+++ src/components/Auth/Login/Login.jsx
@@ -7,67 +7,91 @@ import FormControl from "@mui/material/FormControl";
 import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
 import { useState } from "react";
 import styles from "./Login.module.css";
-
+import { Box } from "@mui/material";
+import { useForm } from "../../../hooks/useForm";
 
 export const Login = () => {
   const [showPassword, setShowPassword] = useState(false);
+  const initialLoginValues = {
+    username: "",
+    password: "",
+  };
+  const { formValues, onChangeHandler, clearFormValues } =
+    useForm(initialLoginValues);
 
   const handleClickShowPassword = () => setShowPassword((show) => !show);
 
   const handleMouseDownPassword = (e) => {
     e.preventDefault();
+    
   };
 
   const handleMouseUpPassword = (e) => {
     e.preventDefault();
   };
 
+  const onClickLoginHandler = (e) => {
+    e.preventDefault();
+    console.log(formValues);
+    clearFormValues()
+  };
+
   return (
     <div className={styles["login-container"]}>
       <h3>Login form</h3>
 
-      <FormGroup>
-        <FormControl sx={{ m: 1, width: "25ch" }} variant="outlined">
-          <InputLabel htmlFor="outlined-adornment-password">
-            Tnet username
-          </InputLabel>
-          <OutlinedInput
-            id="outlined-adornment-username"
-            type="text"
-            endAdornment={
-              <AccountCircleIcon position="end"></AccountCircleIcon>
-            }
-            label="Password"
-          />
-        </FormControl>
-        <FormControl sx={{ m: 1, width: "25ch" }} variant="outlined">
-          <InputLabel htmlFor="outlined-adornment-password">
-            Password
-          </InputLabel>
-          <OutlinedInput
-            id="outlined-adornment-password"
-            type={showPassword ? "text" : "password"}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                  onMouseUp={handleMouseUpPassword}
-                  edge="end"
-                >
-                  {showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-            label="Password"
-          />
-        </FormControl>
-        <Button variant="contained">Log in</Button>
-      </FormGroup>
+      <Box component="form" onSubmit={onClickLoginHandler}>
+        <FormGroup>
+          <FormControl sx={{ m: 1, width: "25ch" }} variant="outlined" required>
+            <InputLabel htmlFor="outlined-adornment-password">
+              Tnet Username
+            </InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-username"
+              type="text"
+              endAdornment={
+                <AccountCircleIcon position="end"></AccountCircleIcon>
+              }
+              label="username"
+              onChange={onChangeHandler}
+              name="username"
+              value={formValues.username}
+            />
+          </FormControl>
+          <FormControl sx={{ m: 1, width: "25ch" }} variant="outlined" required>
+            <InputLabel htmlFor="outlined-adornment-password">
+              Password
+            </InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-password"
+              type={showPassword ? "text" : "password"}
+              name="password"
+              value={formValues.password}
+              onChange={onChangeHandler}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    onMouseUp={handleMouseUpPassword}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              label="Password"
+            />
+          </FormControl>
+          <Button variant="contained" type="submit">
+            Log in
+          </Button>
+        </FormGroup>
+      </Box>
     </div>
   );
 };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,45 +1,36 @@
-
-import styles from './Header.module.css';
-import { Link } from 'react-router-dom';
-import logo from '../../assets/sgs-logo.png'
+import styles from "./Header.module.css";
+import { Link } from "react-router-dom";
+import logo from "../../assets/sgs-logo.png";
 
 const Header = () => {
+  return (
+    <div className={styles["header-container"]}>
+      <div className={styles["logo-container"]}>
+        <img src={logo} alt="SGS" />
+      </div>
+      <div className={styles["menu-container"]}>
+        <ul>
+          <li>
+            <Link to={"/"}>Home</Link>
+          </li>
 
-return (
-    <div className={styles['header-container']}>
-        <div className={styles['logo-container']}>
-            <img src={logo} alt="SGS" />
-        </div>
-        <div className={styles['menu-container']}>
-            <ul>
-                <li >
-                    <Link to={'/'}>Home</Link>
-                </li>
-                
-                <li>
-                    <Link to={'/dashboard'}>Menu</Link>
-                </li>
-                <li>
-                    <Link to={'/auth/login'}>Login</Link>
-                </li>
-                <li>
-                    <Link>Register</Link>
-                </li>
-                <li>
-                    <Link>Logout</Link>
-                </li>
-                <li>
-                    <Link>About</Link>
-                </li>
-            </ul>
-        </div>
+          <li>
+            <Link to={"/dashboard"}>Menu</Link>
+          </li>
+          <li>
+            <Link to={"/auth/login"}>Login</Link>
+          </li>
 
+          <li>
+            <Link>Logout</Link>
+          </li>
+          <li>
+            <Link>About</Link>
+          </li>
+        </ul>
+      </div>
     </div>
-
-
-)
-
-
-}
+  );
+};
 
 export default Header;
